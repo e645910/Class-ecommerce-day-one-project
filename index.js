5fe129da1acdbd07b3b71276d3308907e1937788
Jason Towner <jtowner@ebay.com>
2014-10-21 12:34:55 -0600

    Added migrations to seed customer data

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 "use strict";
+
+var Mongoose = require('mongoose');
+
+var mongoUri = 'mongodb://localhost:27017/ecom';
+Mongoose.connect(mongoUri);
+
+var connection = Mongoose.connection;
+connection.once('open', function(){
+	console.log('Successfully connected to: ' + mongoUri)
+});
+
+global.mongooseConnection = connection;
